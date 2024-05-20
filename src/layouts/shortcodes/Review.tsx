a158a7e52676b72fcd586ff03c2ad728d6709034
ianderthal <gallard3@gmail.com>
2024-05-19 20:25:35 -0400

    build out review component

--- src/layouts/shortcodes/Review.tsx
+++ src/layouts/shortcodes/Review.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 
 const Review = ({
+    tagline,
+    description,
     reviewer,
     score,
 }: {
+    tagline: string;
+    description: string;
     reviewer: string;
     score: string;
 }) => {
   return (
     <>
-      <p>Name: {reviewer}</p>
+      <p>Tagline: "{tagline}"</p>
+      <p>Description: {description}</p>
+      <p>Reviewer: {reviewer}</p>
       <p>Score: {score}</p>
     </>
   );
