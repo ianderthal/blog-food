f110d9e7ef163272cce3ed3a947437152ed39179
ianderthal <gallard3@gmail.com>
2024-06-06 18:37:00 -0400

    style review component

--- src/layouts/shortcodes/Review.tsx
+++ src/layouts/shortcodes/Review.tsx
@@ -13,7 +13,9 @@ const Review = ({
 }) => {
   return (
     <>
-    <section className="m-auto flex items-center gap-4 rounded bg-slate-50 p-4 shadow-xl shadow-slate-500">
+    <h3>Our Reviews</h3>
+    <hr/>
+    <section className="m-auto flex items-center gap-4 rounded bg-slate-50 p-8 shadow-xl shadow-slate-500">
       <div className="flex flex-row">
         <div className="flex flex-col gap-0 basis-1/5 justify-start">
           <figure className="h-12 w-12 flex items-center justify-center rounded-full bg-red-500">
@@ -21,7 +23,7 @@ const Review = ({
           </figure>
         </div>
         <div className="flex flex-col gap-0">
-          <div className="text-2xl font-extrabold">"{tagline}"</div>
+          <div className="text-2xl font-extrabold italic">"{tagline}"</div>
           <p className="mt-2 mb-2">{description}</p>
           <figcaption>
             <div className="font-black uppercase">{reviewer}</div>
