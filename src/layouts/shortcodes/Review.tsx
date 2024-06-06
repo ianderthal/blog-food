24abc3762184757de539809df988d9a840d75770
Ian Gallardo <gallard3@gmail.com>
2024-06-05 21:36:46 -0400

    style review component

--- src/layouts/shortcodes/Review.tsx
+++ src/layouts/shortcodes/Review.tsx
@@ -13,14 +13,23 @@ const Review = ({
 }) => {
   return (
     <>
-      <figure>
-        <p>Score: {score}</p>
-      </figure>
-      <p>Tagline: "{tagline}"</p>
-      <p>Description: {description}</p>
-      <figcaption>
-        <p>Reviewer: {reviewer}</p>
-      </figcaption>
+    <section className="m-auto flex items-center gap-4 rounded bg-slate-50 p-4 shadow-xl shadow-slate-500">
+      <div className="flex flex-row">
+        <div className="flex flex-col gap-0">
+          <figure className="h-12 w-12 items-center justify-center rounded-full bg-green-300">
+            <figcaption>{score}</figcaption>
+          </figure>
+        </div>
+        <div className="flex flex-col gap-0">
+          <div className="text-2xl font-extrabold">"{tagline}"</div>
+          <p className="mt-2 mb-2">{description}</p>
+          <figcaption>
+            <div className="font-black uppercase">{reviewer}</div>
+            <div className="text-sm font-medium">Long live the king</div>
+          </figcaption>
+        </div>
+      </div>
+    </section>
     </>
   );
 };
